0852828c793c339cdb12f40f5302a373ac3d9612
Boburmirzo <boburforfun@gmail.com>
2025-01-19 15:37:07 +0500

    testing

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
+	"github.com/myselfBZ/interpreter/internal/evaluator"
 	"github.com/myselfBZ/interpreter/internal/lexer"
+	"github.com/myselfBZ/interpreter/internal/object"
+	"github.com/myselfBZ/interpreter/internal/parser"
 )
 
 func open(path string) string {
@@ -21,7 +25,13 @@ func open(path string) string {
 }
 
 func main() {
+    env := object.NewEnviroment()
 	src := open("test.monkey")
 	l := lexer.New(src)
-	l.Tokenize()
+    p := parser.New(l)
+    program := p.ParseProgram()
+    o := evaluator.Eval(program, env)
+    if o != nil{
+        fmt.Println(o.Inspect())
+    }
 }
--- test.monkey
+++ test.monkey
@@ -1,1 +1,9 @@
-let boburmirzo = 12;
+let x = 12;
+let t = 2;
+let b = 4;
+if (x > t){
+    return x + t;
+} else {
+    return 0;
+}
+
